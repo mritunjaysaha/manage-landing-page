1bf12e2849dcfe1333a7447b33f9527dc00198a5
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-09-11 20:03:19 +0530

    added testimonials slider

--- pages/index.js
+++ pages/index.js
@@ -5,32 +5,28 @@ import AboutPage from "../src/sections/aboutPage";
 import TestimonialPage from "../src/sections/testimonialsPage";
 import Banner from "../src/sections/banner";
 import Footer from "../src/sections/footerSection";
-const testimonialData = [
-    {
-        id: 1,
+const testimonialData = {
+    0: {
         name: "Anisha Li",
         text:
             "“Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”",
     },
-    {
-        id: 2,
+    1: {
         name: "Ali Bravo",
         text:
             "“We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.”",
     },
-    {
-        id: 3,
+    2: {
         name: "Richard Watts",
         text:
             "“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”",
     },
-    {
-        id: 4,
+    3: {
         name: "Shanai Gough",
         text:
             "“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”",
     },
-];
+};
 
 function Index() {
     return (
--- src/sections/testimonialsPage.js
+++ src/sections/testimonialsPage.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/buttons";
+
+function Testiomonials({ data }) {
+    const [current, setCurrent] = useState(data[0]);
+    const [active, setActive] = useState(0);
+    function handleSetClick(e) {
+        const selected = e.target.dataset["testimonial"];
+
+        setCurrent(data[selected]);
+
+        document
+            .querySelector(`span[data-testimonial='${selected}']`)
+            .classList.add("active");
+    }
+    return (
+        <>
+            <section className="testimonials-cards">
+                <div className="dp"></div>
+                <h5>{current.name}</h5>
+                <p>{current.text}</p>
+            </section>
+            <section className="span-testimonials-container">
+                {Object.keys(data).map((index) => {
+                    {
+                        return (
+                            <span
+                                className="span-testimonials-cards"
+                                onClick={(e) => handleSetClick(e)}
+                                data-testimonial={index}
+                                key={index}
+                            />
+                        );
+                    }
+                })}
+            </section>
+        </>
+    );
+}
 
 export default function TestimonialPage({ data }) {
     return (
@@ -7,12 +44,7 @@ export default function TestimonialPage({ data }) {
             <header className="header">
                 <h3>What they've said</h3>
             </header>
-
-            <div className="testimonials-cards">
-                <div className="dp"></div>
-                <h5>{data[0].name}</h5>
-                <p>{data[0].text}</p>
-            </div>
+            <Testiomonials data={data} />
 
             <Button text="Get Started" />
         </section>
