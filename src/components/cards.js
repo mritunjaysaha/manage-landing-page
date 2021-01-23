43953a920525f13cf1c1c9c31cf6af3b84e649c9
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-01-24 01:05:01 +0530

    completed about page section for mobile view

--- src/components/cards.js
+++ src/components/cards.js
@@ -3,11 +3,17 @@ import React from "react";
 export default function Card({ id, header, text }) {
     return (
         <article className="cards">
-            <div className="tag">
+            <div className="tag hide-on-mobile">
                 <p>{id}</p>
             </div>
             <div className="contents">
-                <h4 className="contents-header">{header}</h4>
+                <h4 className="contents-header hide-on-mobile">{header}</h4>
+                <div className="id-header-div hide-on-desktop">
+                    <div className="tag">
+                        <p>{id}</p>
+                    </div>
+                    <h4>{header}</h4>
+                </div>
                 <p className="contents-text">{text}</p>
             </div>
         </article>
