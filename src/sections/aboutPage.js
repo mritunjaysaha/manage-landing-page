b656275b7ba936e83df54e098e3a39de056710ca
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-01-24 10:26:50 +0530

    updated contents of about page

--- src/sections/aboutPage.js
+++ src/sections/aboutPage.js
@@ -9,6 +9,18 @@ export default function AboutPage() {
             text:
                 "See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.",
         },
+        {
+            id: "02",
+            header: "Advanced built-in reports",
+            text:
+                "Set interval delivery estimates and track progress toward company goal. Our customizable dashboard helps you build out the reports you need to keep key stakeholders informed.",
+        },
+        {
+            id: "03",
+            header: "Everything you need in one place",
+            text:
+                "Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.",
+        },
     ];
     return (
         <section className="about">
@@ -21,22 +33,9 @@ export default function AboutPage() {
                 </p>
             </article>
             <article className="about page-right">
-                <Card
-                    id={data[0].id}
-                    header={data[0].header}
-                    text={data[0].text}
-                />
-
-                <Card
-                    id={data[0].id}
-                    header={data[0].header}
-                    text={data[0].text}
-                />
-                <Card
-                    id={data[0].id}
-                    header={data[0].header}
-                    text={data[0].text}
-                />
+                {data.map(({ id, header, text }) => (
+                    <Card id={id} header={header} text={text} />
+                ))}
             </article>
         </section>
     );
