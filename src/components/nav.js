2343c6598245a35e19ccb39bfd708d3b3968f369
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-01-25 20:02:02 +0530

    added function to open and close the hamburger menu

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,23 +1,70 @@
 import React from "react";
 import Button from "./buttons";
-import Hamburger from "../sections/hamburger-menu";
+import Anchor from "./anchor";
 
 export default function Navbar() {
+    function get(element) {
+        return document.querySelector(element);
+    }
+
+    function handleMenu() {
+        const hamburgerIconDiv = get(".hamburger-icon-div");
+        const hamburgerCloseP = get(".hamburger-close-div");
+        const hamburgerMenuDiv = get(".hamburger-menu-div");
+        const menu = get("#menu");
+        hamburgerIconDiv.style.display = "none";
+        hamburgerCloseP.style.display = "block";
+        hamburgerMenuDiv.style.display = "block";
+
+        console.log({ menu });
+        menu.classList.add("open-menu");
+    }
+
+    function handleClose() {
+        const hamburgerIconDiv = get(".hamburger-icon-div");
+        const hamburgerCloseP = get(".hamburger-close-div");
+        const hamburgerMenuDiv = get(".hamburger-menu-div");
+        const menu = get("#menu");
+
+        hamburgerIconDiv.style.display = "flex";
+        hamburgerCloseP.style.display = "none";
+        hamburgerMenuDiv.style.display = "none";
+        menu.classList.remove("open-menu");
+    }
+
     return (
         <header>
             <nav className="navbar">
                 <div className="logo"></div>
 
-                <ul className="navbar-mid-contents">
-                    <li>Pricing</li>
-                    <li>Product</li>
-                    <li>About Us</li>
-                    <li>Careers</li>
-                    <li>Community</li>
-                </ul>
+                <div onClick={handleMenu} className="hamburger-icon-div">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="hamburger-close-div">&times;</div>
+
+                <div onClick={handleClose} className="hamburger-menu-div">
+                    <ul id="menu" className="navbar-mid-contents">
+                        <li>
+                            <Anchor name="pricing" link="#" />
+                        </li>
+                        <li>
+                            <Anchor name="product" link="#" />
+                        </li>
+                        <li>
+                            <Anchor name="about us" link="#" />
+                        </li>
+                        <li>
+                            <Anchor name="careers" link="#" />
+                        </li>
+                        <li>
+                            <Anchor name="community" link="#" />
+                        </li>
+                    </ul>
+                </div>
 
                 <Button text="Get Started" />
-                <Hamburger />
             </nav>
         </header>
     );
